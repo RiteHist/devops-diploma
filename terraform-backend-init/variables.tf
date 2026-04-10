f84c9ba4dcc5496825536a4517458f92dfe95e2d
RiteHist <greggthewright@gmail.com>
2026-04-10 18:27:02 +1000

    Add basic skeleton for init of terraform backend

---
+++ terraform-backend-init/variables.tf
@@ -0,0 +1,16 @@
+variable "token" {
+    type = string
+    sensitive = true
+}
+
+variable "cloud_id" {
+    type = string
+}
+
+variable "folder_id" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
